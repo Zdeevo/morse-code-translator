e1dd17f3ac406cd342e10746d863069665602ab2
Steve Maguire <dannyblack42@protonmail.com>
2020-10-02 10:45:07 +0100

    html and css done, working on js

---
+++ dist/main.dev.js
@@ -0,0 +1,9 @@
+// create a website that translates English to Morse code
+// forget about the extension
+// 1. store english letters and morse code for use by the translator
+// 2. take inputed english from the input field
+// 3. event listener for the button click
+// 4. do the thing:
+//    a. get each letter and output corresponding morse letter
+//    b. do we need a character for spaces between words??
+"use strict";
